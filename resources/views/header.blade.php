d70bfbcdbac51c1c86ec836a24cb3947a341f9f6
AliasTzr <94296101+AliasTzr@users.noreply.github.com>
2022-01-25 22:52:55 +0000

    Add files via upload

---
+++ resources/views/header.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="administrate/resources/css/app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg" style="background-color: #156a9b; color:white">
+            <div class="container-fluid">
+              <a class="navbar-brand" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><i class="bi bi-menu-button-wide-fill fs-3 text-light"></i></a>
+              <div class="col pt-3 fs-6 fw-bold">
+                  <p>CONNECT DASHBOARD</p>
+              </div>
+            </div>
+          </nav>
+          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header p-4 mb-2" style="background-color: #156a9b;">
+              <h5 class="offcanvas-title m-auto" id="offcanvasExampleLabel">DASHBOARD MENU</h5>
+              <button class="border border-0 bg-danger rounded-3" data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-x-lg fs-"></i></button>
+            </div>
+            <div class="offcanvas-body pe-1 ps-0">
+              <div class="dropdown mt-3">
+                <div class="bg-primary p-3 mb-1">
+                    item 1
+                </div>
+                <div class="bg-primary p-3 mb-1">
+                    item 2
+                </div>
+                <div class="bg-primary p-3 mb-1">
+                    item 3
+                </div>
+                <div class="bg-primary p-3 mb-1">
+                    item 4
+                </div>
+              </div>
+            </div>
+          </div>
+    </header>
+    <main>
---
+++ storage/framework/views/c20a64888897bd1e65516cb5be98a7da2177440f.php
@@ -0,0 +1,6 @@
+<?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+Ajdjdjjdd
+
+
+<?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\oulohade\Documents\TAGRAUD\administrate\resources\views/welcome.blade.php ENDPATH**/ ?>
